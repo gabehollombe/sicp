0dcc2ac5258720fb9953fb439776646a92c316c2
Gabe Hollombe <gabe@avantbard.com>
2014-10-10 04:56:46 -0400

    fix 1.18

--- 1.18.scm
+++ 1.18.scm
@@ -8,7 +8,8 @@
   (helper a b 0))
 
 (define (helper a b product)
-  (cond ((= 0 b) (- product a))
+  (cond ((= 1 b) product)
+        ((= 0 b) 0)
         ((even? b) (helper a (half b) (+ (double a) product)))
         (else (helper a (- b 1) (+ a product)))))
 
